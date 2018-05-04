5c7384725fc1d7f3e95d338b40c763539ab78240
bsc2012 <443004709@qq.com>
2018-05-04 18:01:56 +0800

    20180504

--- bsc-spring-cloud/bsc-spring-cloud-controller/bsc-spring-cloud-controller-rest/bsc-spring-cloud-controller-rest-hello/src/main/java/com/bsc/spring/cloud/controller/rest/hello/HelloController.java
+++ bsc-spring-cloud/bsc-spring-cloud-controller/bsc-spring-cloud-controller-rest/bsc-spring-cloud-controller-rest-hello/src/main/java/com/bsc/spring/cloud/controller/rest/hello/HelloController.java
@@ -1,12 +1,14 @@
 package com.bsc.spring.cloud.controller.rest.hello;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.bsc.spring.cloud.inf.feign.hello.IHelloService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class HelloController {
@@ -21,10 +23,18 @@ public class HelloController {
     private RestTemplate restTemplate;
 	
     @RequestMapping(value = "/ribbonHello")
+    @HystrixCommand(fallbackMethod = "ribbonHelloError")
     public String ribbonHello(@RequestParam String name){
     	System.out.println("ribbonHello service consumer");
         return restTemplate.getForObject("http://SERVICE-HELLO/hello?name="+name,String.class);
     }
+    
+    public String ribbonHelloError(String name){
+    	return "sorry " + name;
+    }
+    
+    @Value("${foo}")
+    private String foo;
     
     @Autowired(required=false)
     private IHelloService helloService;
@@ -32,6 +42,7 @@ public class HelloController {
     @RequestMapping(value = "/feignHello")
     public String feignHello(@RequestParam String name){
     	System.out.println("feignHello service consumer");
+    	System.out.println("foo:" + foo);
         return helloService.hello(name);
     }
 }
