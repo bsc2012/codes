87bcb261ef84a83d18e4361d385a198593dd5fdc
bsc2012 <443004709@qq.com>
2018-06-28 16:39:27 +0800

    20180628

--- bsc-algorithm/bsc-algorithm-sort/bsc-algorithm-sort-select/src/main/java/com/bsc/algorithm/sort/select/SelectSort.java
+++ bsc-algorithm/bsc-algorithm-sort/bsc-algorithm-sort-select/src/main/java/com/bsc/algorithm/sort/select/SelectSort.java
@@ -11,10 +11,10 @@ public class SelectSort<T extends Comparable<T>> extends AbstractSort<T> {
 
 	@Override
 	/**
-	 * 找出第0到第data.length - 1个元素中最小值，和第0个元素交换
-	 * 找出第1到第data.length - 1个元素中最小值，和第1个元素交换
+	 * 找出第1个到最后一个元素中最小值，与第1个交换
+	 * 找出第2个到最后一个元素中最小值，与第2个交换
 	 * ...
-	 * 找出第data.length - 2到第data.length - 1个元素中最小值，和第data.length - 2个元素交换
+	 * 找出倒数第2个与最后一个元素中最小值，与倒数第2个交换
 	 */
 	protected void sort(T[] data, int cr) {
 		int length = data.length;
@@ -22,18 +22,18 @@ public class SelectSort<T extends Comparable<T>> extends AbstractSort<T> {
 		for (int i = 0; i < length - 1; i++) {
 			//最小值
 			T minValue = data[i];
-			//最大或最小值数组座标
+			//最小值数组座标
 			int minValueIndex = i;
 			for (int j = i + 1; j < length; j++) {
 				if (compare(minValue, data[j]) == cr) {
 					//记录最大值或最小值
 					minValue = data[j];
-					//记录最大值或最小值数组座标
+					//记录最小值数组座标
 					minValueIndex = j;
 				}
 			}
 			if(minValueIndex > i){
-				//把最大值或最小值和第i个元素交换
+				//把最小值和第i个元素交换
 				data[minValueIndex] = data[i];
 				data[i] = minValue;
 			}
